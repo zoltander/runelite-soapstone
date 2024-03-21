375db70009d7a2e8d9d4232124320d2f53024b01
Zoltan Toth <88989745+zoltander@users.noreply.github.com>
2024-03-21 17:37:19 -0400

    started plugin slate

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# SOAPSTONE
+made you look
--- src/main/java/com/zoltan/soapstone/SoapstonePlugin.java
+++ src/main/java/com/zoltan/soapstone/SoapstonePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.zoltan.soapstone;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Soapstone"
 )
-public class ExamplePlugin extends Plugin
+public class SoapstonePlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private SoapstoneConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	SoapstoneConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(SoapstoneConfig.class);
 	}
 }
